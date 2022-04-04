1a92fc7713631cdc9c4b8a32a34612e634f0308e
Daniel Furman <djfurman@gmail.com>
2022-04-03 20:36:19 -0400

    Refine message.

--- src/stores/messages.js
+++ src/stores/messages.js
@@ -5,12 +5,21 @@ export const useMessageStore = defineStore({
   state: () => ({
     messages: [
         {
+            conversation_id: '0144e876-7985-47fe-becb-acb78d24261b:acc2431b-da4d-46b3-9a49-fc3445c909cc:2',
             subject: 'Secure Message',
             from: 'Dr. Green',
             at: new Date("2022-03-21T17:13:51"),
             body: 'Lab results are in everything looks great, let me know if there any issues, otherwise I will see you at your next appointment.',
             is_read: false,
-        }
+        },
+        {
+          conversation_id: '0144e876-7985-47fe-becb-acb78d24261b:acc2431b-da4d-46b3-9a49-fc3445c909cc:1',
+          subject: 'Test results?',
+          from: 'John Cleese',
+          at: new Date("2020-03-10T09:32:13"),
+          body: 'I know these test can take a while but please let me know when you get the results',
+          is_read: true,
+        },
     ]
   }),
   getters: {
