e37e6ebe94f4976aebd032006502915130de250c
Daniel Furman <djfurman@gmail.com>
2022-05-02 20:57:07 -0600

    Fix patient selection.

--- src/components/PatientSelect.vue
+++ src/components/PatientSelect.vue
@@ -7,8 +7,6 @@ import { computed, ref } from 'vue'
 const userStore = useSimpleUserStore()
 const { isLoggedIn, isAccessPopulated } = storeToRefs(userStore)
 const patientsIdStore = usePatientsIdStore()
-const { patients, selectedPatient } = storeToRefs(patientsIdStore)
-const { getPatientByPatientId } = patientsIdStore
 
 const accessUpdate = userStore.$onAction(({
   name,
@@ -30,49 +28,28 @@ const accessUpdate = userStore.$onAction(({
   }
 })
 
-const buildPatientName = (patientBasicId) => {
-  const basicId = patientBasicId.value
-  if (basicId == undefined) {
-    return null
-  }
-
-  if (!basicId.name.preferred_username.value) {
-    return `${basicId.name.given_name.value} ${basicId.name.family_name.value}`
-  }
-
-  return basicId.preferred_username.value
-}
-
-const currentPatient = computed(() => {
-  return getPatientByPatientId(selectedPatient)
-})
-
-const currentPatientName = computed(buildPatientName((currentPatient)))
-
-const patientName = computed((patient) => {
-  return buildPatientName(patient)
-})
-
 const menuIsActive = ref(false)
 const dynamicClass = computed((menuIsActive) => {
   if (menuIsActive) { return 'is-active' }
 })
 
 const selectPatient = (patientId) => {
-  patientsIdStore.state.selectPatient = patientId
+  patientsIdStore.setActivePatient(patientId)
   menuIsActive.value = !menuIsActive.value
 }
 </script>
 
 <template>
-  <div class="navbar-item has-dropdown is-hoverable" v-if="isLoggedIn && isAccessPopulated">
-    <a class="navbar-link" :class="dynamicClass" @click="menuIsActive = !menuIsActive">Acting As: {{ currentPatientName
+  <div class="navbar-item has-dropdown is-hoverable"
+    v-if="isLoggedIn && isAccessPopulated && patientsIdStore.isPopulated">
+    <a class="navbar-link" :class="dynamicClass" @click="menuIsActive = !menuIsActive">Acting As: {{
+        patientsIdStore.currentPatient.name
     }} </a>
 
     <div class="navbar-dropdown">
-      <div class="navbar-item" v-for="patient in patients" :key="patient.id" @click="selectPatient(patient.id)">{{
-          patientName
-      }}</div>
+      <div class="navbar-item" v-for="patient in patientsIdStore.patients" :key="patient.id">
+        <a @click="selectPatient(patient.id)">{{ patient.name }}</a>
+      </div>
     </div>
   </div>
 </template>
--- src/stores/patientsId.js
+++ src/stores/patientsId.js
@@ -9,58 +9,98 @@ const api = mande('/demo')
 export const usePatientsIdStore = defineStore({
   id: 'patientIds',
   state: () => ({
+    hasLoadErrors: false,
     patients: [],
-    selectedPatient: '',
+    selectedPatientId: '',
     isLoading: false,
+    isPopulated: false,
   }),
   getters: {
+    currentPatient: (state) => {
+      return state.patients.find((patient) => patient.id === state.selectedPatientId)
+    },
+
     getPatientByPatientId: (state) => {
       return (patientId) => {
-        if (!state.isLoading) {
-          state.selectedPatient = 'loading'
-          return {
-            id: 'loading',
-            picture: null,
-            name: {
-              given_name: 'Still',
-              family_name: 'Loading',
-              preferred_username: 'Loading...',
-            }
-          }
-        }
-        state.patients.find((patient) => patient.patientId === patientId)
+        // if (state.isLoading && !state.isPopulated) {
+        //   state.selectedPatientId = 'loading'
+        //   return {
+        //     id: 'loading',
+        //     picture: null,
+        //     name: 'Loading...',
+        //     familyName: 'Loading',
+        //     givenName: 'Still',
+        //     preferredUsername: 'Loading...',
+        //   }
+        // }
+        return state.patients.find((patient) => patient.id === patientId)
       }
     },
   },
   actions: {
+    buildPatientName(patient) {
+      if (patient.preferredUsername) {
+        return patient.preferredUsername
+      }
+      return `${patient.givenName} ${patient.familyName}`
+    },
+
     async fill() {
       this.isLoading = true
       const userStore = useSimpleUserStore()
       const patientIdList = userStore.getPatientIdList()
-      await patientIdList.forEach(async (patientId) => {
-        try {
-          let patientIdRes = await api.get(`/patients/${patientId}/basicId`, userStore.withApiSettings())
-
-          if (patientIdRes.status === 200 || patientIdRes.status === 304) {
-            const patientData = await patientIdRes.json()
-            if (patientData.status === 'success') {
-              const patientInfo = patientData.data
-              this.patients.push({ id: patientId, picture: patientInfo.picture, name: { given_name: patientInfo.name.given_name, family_name: patientInfo.name.family_name, preferred_username: null } })
-              this.selectedPatient = this.patients[0].id
-              this.isLoading = false
-            } else {
-              console.log('jsend status other than success')
-              console.log(patientData)
-            }
-          } else {
-            console.log('api call returned other than success http status')
-            console.log(patientIdRes)
-          }
-        } catch (err) {
-          console.log(err)
-        }
-      })
+      for (let patientId of patientIdList) {
+        await this.fillPatientIdData(patientId, userStore)
+      }
+      this.selectedPatientId = this.patients[0].id
+      this.setAllClear()
     },
+
+    async fillPatientIdData(patientId, userStore) {
+      try {
+        let patientIdRes = await api.get(`/patients/${patientId}/basicId`, userStore.withApiSettings())
+
+        if (patientIdRes.status === 200 || patientIdRes.status === 304) {
+          const patientData = await patientIdRes.json()
+          if (patientData.status === 'success') {
+            const patientInfo = patientData.data
+
+            this.patients.push({
+              id: patientId,
+              picture: patientInfo.picture,
+              familyName: patientInfo.familyName,
+              givenName: patientInfo.givenName,
+              name: this.buildPatientName(patientInfo),
+              preferredUsername: null,
+            })
+          } else {
+            console.log('jsend status other than success')
+            console.log(patientData)
+          }
+        } else {
+          console.log('api call returned other than success http status')
+          console.log(patientIdRes)
+        }
+      } catch (err) {
+        console.log(err)
+      }
+    },
+
+    setActivePatient(patientId) {
+      this.selectedPatientId = patientId
+    },
+
+    setAllClear() {
+      this.hasLoadErrors = false
+      this.isLoading = false
+      this.isPopulated = true
+    },
+
+    setErrorState() {
+      this.hasLoadErrors = true
+      this.isLoading = false
+      this.isPopulated = false
+    }
   }
 })
 
